650fe9f437f0f23ff40ca6acaf50deba227711da
Jürgen Gutsch <juergen@gutsch-online.de>
2013-11-28 22:41:36 +0100

    Aufgabe 01 erledigt

---
+++ Aufgabe01/Webserver/Webserver/Middleware/DownloadMiddlewar.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using ICSharpCode.SharpZipLib.Zip;
+using Microsoft.Owin;
+using Webserver.Infrastructure;
+using Webserver.Options;
+
+namespace Webserver.Middleware
+{
+    class DownloadMiddlewar : OwinMiddleware
+    {
+        private readonly WebserverOptions _options;
+
+        public DownloadMiddlewar(OwinMiddleware next, WebserverOptions options)
+            : base(next)
+        {
+            _options = options;
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            var path = FileSystem.MapPath(_options.BasePath, context.Request.Path.Value);
+            var ext = Path.GetExtension(path);
+            if (CanHandle(ext, context.Request.Path.Value))
+            {
+                Trace.WriteLine("Invoke DownloadMiddlewar");
+
+                var filename = Path.GetFileName(path);
+                var folderPath = Path.Combine(_options.BasePath, "downloads");
+
+                context.Response.StatusCode = 200;
+                context.Response.ContentType = MimeTypeService.GetMimeType(".zip");
+                context.Response.Headers.Add("Content-Disposition", new[] { String.Format("attachment; filename={0}.zip", filename) });
+
+                var fastZip = new FastZip();
+                fastZip.CreateZip(context.Response.Body, folderPath, false, filename, null);
+
+                return Globals.CompletedTask;
+            }
+
+            return Next.Invoke(context);
+        }
+
+        private static bool CanHandle(string ext, string path)
+        {
+            return !String.IsNullOrWhiteSpace(ext) && !ext.Equals(".zip") && path.StartsWith("/downloads/");
+        }
+    }
+}
---
+++ Aufgabe01/Webserver/Webserver/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Owin;
+using Webserver.Middleware;
+using Webserver.Options;
+
+namespace Webserver
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var webserverOptions = new WebserverOptions
+            {
+                BasePath = Environment.CurrentDirectory
+            };
+
+            app.Use(typeof(TraceMiddleware));
+            app.UseHandlerAsync((request, response, next) =>
+            {
+                response.AddHeader("Server", "dotnetpro Webserver");
+                return next();
+            });
+
+            app.Use(typeof(IsPrivateFolderMiddleware), webserverOptions);
+            app.Use(typeof(ResourceExistMiddleware), webserverOptions);
+            app.Use(typeof(DownloadMiddlewar), webserverOptions);
+            app.Use(typeof(LessMiddleware), webserverOptions);
+            app.Use(typeof(SassMiddleware), webserverOptions);
+            app.Use(typeof(MinifyMiddleware), webserverOptions);
+            app.Use(typeof(RazorMiddleware), webserverOptions);
+            app.Use(typeof(DefaultMiddleware), webserverOptions);
+        }
+
+
+
+        private void RegisterAddresses(IAppBuilder app)
+        {
+
+            var hostUri = new Uri("http://localhost:16897/");
+            var hostUriHttps = new Uri("https://localhost:44567/");
+
+            // NOTE: OwinServerFactoryAttribute.Create method and 
+            //       OwinHttpListener.Start should give you a hint on how
+            //       to register the addresses.
+            app.Properties["host.Addresses"] = new List<IDictionary<string, object>> {
+            new Dictionary<string, object> { 
+                { "scheme", hostUri.Scheme },
+                { "host", hostUri.Host },
+                { "port", hostUri.Port.ToString()  }
+            },
+
+            new Dictionary<string, object> { 
+                { "scheme", hostUriHttps.Scheme },
+                { "host", hostUriHttps.Host },
+                { "port", hostUriHttps.Port.ToString()  }
+            }
+        };
+        }
+
+    }
+
+}
